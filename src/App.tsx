12e5ecdea7dbd0997ed0032fc2584f4de27ba41a
Oscar Serrano Yanez <2691820@tcs.com>
2025-11-24 11:44:04 -0600

    Creacion de proyecto base

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import FormComponent from './components/form-component/FormComponent'
+import ListComponent from './components/list-component/ListComponent'
+
+function App() {
+
+  return (
+   <>
+   <FormComponent />
+   <ListComponent />
+   
+  </>
+  )
+}
+
+export default App
---
+++ src/components/list-component/ListComponent.tsx
@@ -0,0 +1,17 @@
+
+function ListComponent () {
+  return (
+  <>
+  <ul>
+    <li>item1</li>
+    <li>item2</li>
+    <li>item3</li>
+    <li>item4</li>
+    <li>item5</li>
+    <li>item6</li>
+  </ul>
+  </>
+  )
+}
+
+export default ListComponent
